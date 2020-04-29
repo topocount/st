fd95422302bc262b6bc58710fef888d7ba63c5ac
Randoragon <randoragongamedev@gmail.com>
2020-04-29 20:25:30 +0200

    Apply externalpipe patch and add openurl script
    In the future all st-related scripts must be prefixed with "st-script-"

--- config.h
+++ config.h
@@ -177,6 +177,9 @@ static MouseShortcut mshortcuts[] = {
 #define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
+/* use externalpipe to pipe links to dmenu */
+static char *openurlcmd[] = { "/bin/sh", "-c", "st-script-openurl", "externalpipe", NULL };
+
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
@@ -191,6 +194,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ MODKEY, 				XK_u, 			externalpipe, 	{ .v = openurlcmd } },
+	{ MODKEY, 				XK_t, 			externalpipe, 	{ .v = rofitest } },
 };
 
 /*
