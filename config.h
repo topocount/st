4b3ca5b60c55ad65c23c947c7a12ee34034ffbaa
Randoragon <randoragongamedev@gmail.com>
2020-04-30 20:34:35 +0200

    Improve color integration with color schemes
    Removed the additional color indices past [255].

--- config.h
+++ config.h
@@ -151,9 +151,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
+static unsigned int defaultcs = 15;
 static unsigned int defaultrcs = 257;
 
 /*
